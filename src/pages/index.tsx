19686b877c387b60f4d7fef82d833bcdad60036f
realsammy <160411460+darealsammy@users.noreply.github.com>
2026-07-21 21:32:27 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import type {ReactNode} from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
 import styles from './index.module.css';
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -30,7 +27,6 @@ function HomepageHeader() {
     </header>
   );
 }
-
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
